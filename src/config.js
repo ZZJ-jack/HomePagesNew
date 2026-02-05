e41c6aba280d81c41da55b36a63bd519a4311135
ZZJ-JACK <zzjjack@zzjjack.us.kg>
2026-02-05 20:13:36 +0800

    feat: 添加植物大战僵尸游戏链接到配置
    在项目配置中添加植物大战僵尸游戏的链接信息，包括标题、描述、图片和URL，以便在页面上展示

--- src/config.js
+++ src/config.js
@@ -143,6 +143,7 @@ const config = {
 		{ go: "前往", img: "/img/gocoding-logo.png", title: "Coding", subtitle: "Coding团队的主页", text: "在Coding，见到更好的自己", url: "https://gocoding.dpdns.org", show: false },
 		{ go: "前往", img: "/img/spy-logo.png", title: "谁是卧底", subtitle: "谁是卧底出题工具", text: "谁是卧底，第一个前后端都有的项目", url: "https://spy.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/truthwords-logo.png", title: "真心话大冒险", subtitle: "真心话大冒险出题工具", text: "真心话大冒险，一个简单却有趣的游戏", url: "https://truthwordstools.zzjjack.dpdns.org", show: false },
+		{ go: "前往", img: "/img/pvz-logo.jpg", title: "植物大战僵尸", subtitle: "用python写的植物大战僵尸游戏", text: "植物大战僵尸，一个经典的游戏", url: "https://pvz.zzjjack.us.kg", show: false },
 	],
 
 	statement: ["Copyright © 2026 ZZJ"],

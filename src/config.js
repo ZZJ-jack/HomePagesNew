e06514d8c22e5e84bb525138cf46b383e4454c92
ZZJ-JACK <zzjjack@zzjjack.us.kg>
2026-05-02 13:00:01 +0800

    feat: 添加网站图标和SEO优化文件
    添加多种尺寸的favicon图标和PWA相关文件
    更新gitee图标格式并添加robots.txt和sitemap.xml
    在HTML中添加隐藏的h1标签以优化SEO

--- src/config.js
+++ src/config.js
@@ -145,7 +145,7 @@ const config = {
 		{ go: "前往", img: "/img/chat-logo.jpeg", title: "在线实时聊天室", subtitle: "在线聊天室", text: "ZZJ的聊天室，提供实时聊天功能", url: "https://chat.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/copy-logo.jpg", title: "云剪切板", subtitle: "在线云剪切板工具", text: "云剪切板，一个简单的工具", url: "https://copy.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/truthwords-logo.png", title: "真心话大冒险", subtitle: "真心话大冒险出题工具", text: "真心话大冒险，一个简单却有趣的游戏", url: "https://truthwordstools.zzjjack.dpdns.org", show: false },
-		{ go: "前往", img: "/img/gitee-logo.png", title: "Gitee代码获取", subtitle: "获取luogu的AC代码", text: "本工具会在Gitee上获取luogu的AC代码", url: "https://getzsyzcode.zzjjack.us.kg", show: false },
+		{ go: "前往", img: "/img/gitee-logo.jpg", title: "Gitee代码获取", subtitle: "获取luogu的AC代码", text: "本工具会在Gitee上获取luogu的AC代码", url: "https://getzsyzcode.zzjjack.us.kg", show: false },
 	],
 
 	statement: ["Copyright © 2026 ZZJ"],

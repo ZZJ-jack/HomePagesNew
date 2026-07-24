5b06de64f193e2b518f1b536b49eb3ff4132e938
ZZJ-JACK <zzjjack@zzjjack.us.kg>
2026-07-24 20:12:18 +0800

    fix(config): 调整项目卡片中Just-Solo的位置
    将Just-Solo项目卡片从列表末尾移至最前方，优化展示顺序

--- src/config.js
+++ src/config.js
@@ -140,13 +140,13 @@ const config = {
 	//项目卡片 其中 字段"show"控制初始卡片的text是否展开
 	projectcards: [
 		{ go: "前往", img: "/img/gocoding-logo.png", title: "Coding", subtitle: "Coding团队的主页", text: "在Coding，见到更好的自己", url: "https://gocoding.cc.cd", show: false },
+		{ go: "前往", img: "/img/Just-Solo.ico", title: "Just-Solo", subtitle: "Just-Solo音乐播放器", text: "Just-Solo，一个高性能、低占用的音乐播放器", url: "https://gitcode.com/ZZJ-JACK/Just-Solo", show: false },
 		{ go: "前往", img: "/img/pvz-logo.jpg", title: "植物大战僵尸", subtitle: "用python写的植物大战僵尸游戏", text: "植物大战僵尸，一个经典的游戏", url: "https://pvz.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/spy-logo.png", title: "谁是卧底", subtitle: "谁是卧底出题工具", text: "谁是卧底，第一个前后端都有的项目", url: "https://spy.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/chat-logo.jpeg", title: "在线实时聊天室", subtitle: "在线聊天室", text: "ZZJ的聊天室，提供实时聊天功能", url: "https://chat.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/copy-logo.jpg", title: "云剪切板", subtitle: "在线云剪切板工具", text: "云剪切板，一个简单的工具", url: "https://copy.zzjjack.us.kg", show: false },
 		{ go: "前往", img: "/img/truthwords-logo.png", title: "真心话大冒险", subtitle: "真心话大冒险出题工具", text: "真心话大冒险，一个简单却有趣的游戏", url: "https://truthwordstools.zzjjack.dpdns.org", show: false },
 		{ go: "前往", img: "/img/gitee-logo.jpg", title: "Gitee代码获取", subtitle: "获取luogu的AC代码", text: "本工具会在Gitee上获取luogu的AC代码", url: "https://getzsyzcode.zzjjack.us.kg", show: false },
-		{ go: "前往", img: "/img/Just-Solo.ico", title: "Just-Solo", subtitle: "Just-Solo音乐播放器", text: "Just-Solo，一个高性能、低占用的音乐播放器", url: "https://gitcode.com/ZZJ-JACK/Just-Solo", show: false },
 	],
 
 	statement: ["Copyright © 2026 ZZJ"],
